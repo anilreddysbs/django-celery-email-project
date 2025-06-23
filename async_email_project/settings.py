70bc3d2ec5e2ee055121d5723f2ce012956f778a
Sabbella Bhagya Sai Anil Reddy <105065141+anilreddysbs@users.noreply.github.com>
2025-06-23 12:09:51 +0530

    Update settings.py

--- async_email_project/settings.py
+++ async_email_project/settings.py
@@ -2,7 +2,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'your-secret-key'
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
